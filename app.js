34416108b9a24671576697885f668df601ce5dfc
Veljko <veljkoBitno@gmail.com>
2023-01-31 19:39:30 +0100

    Finished email error handling

--- app.js
+++ app.js
@@ -1,10 +1,31 @@
-const handleErrors = () => {};
+let hasNoErrors = false;
+
+const handleErrors = (input) => {
+  const reg = /^[\w\-\.\+]+\@[a-zA-Z0-9\.\-]+\.[a-zA-z0-9]{2,4}$/;
+
+  if (input.value.match(reg)) {
+    hasNoErrors = true;
+  } else {
+    hasNoErrors = false;
+  }
+};
 
 const handleForm = (() => {
   const form = document.querySelector(".content__form");
-  const error = document.querySelector(".content__form__error");
-  const input = form.children[0].value;
+  const error = document.querySelector(".error");
+  const input = form.children[0];
+
   form.addEventListener("submit", (e) => {
-    e.preventDefault();
+    handleErrors(input);
+
+    if (hasNoErrors) {
+      error.style.display = "none";
+    } else {
+      e.preventDefault();
+      error.style.display = "block";
+      form.style.border = "2px solid #F96464";
+      error.textContent = "Please provide a valid email";
+      input.classList.add("img-error");
+    }
   });
 })();
